065ab4e7e6f0daf5b97ebdba6affed4ee166e58c
licine <112944481+licine@users.noreply.github.com>
2023-11-15 09:40:02 +0800

    Delete src/Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author shkstart
- * @create ${YEAR}-${MONTH}-${DAY} ${TIME}
- */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
